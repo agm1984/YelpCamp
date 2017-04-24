973387d7af97adc3f513b7b71ad9c14dc6f9e566
Adam Mackintosh <agm1984@gmail.com>
2017-04-24 00:03:51 -0700

    added bodyParser

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
 
-// Set public asset directory and template engine
-//app.use(express.static('views'));
+// Use bodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set view template engine
 app.set('view engine', 'ejs');
 
 // Landing Route
 app.get('/', function(req, res) {
     res.render('landing');
+});
+
+// Campground Route
+app.get('/campgrounds', function(req, res) {
+    var campgrounds = [
+        { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg" },
+        { name: "Granite Hill", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" },
+        { name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg" }
+    ];
+    res.render('campgrounds', { campgrounds: campgrounds });
+});
+app.post('/campgrounds', function(req, res) {
+    res.send('you hit the campgrounds post');
+    // get data from form and add to campgrounds array
+    // redirect back to campgrounds page
 });
 
 // Server Config
